5eaec778c49c8d4cae6968b3e76e7ab0c2943ead
abusamin44 <abusamin44@gmail.com>
2026-04-26 15:06:09 +0600

    fix errors

--- server/server.js
+++ server/server.js
@@ -24,10 +24,6 @@ app.get('/api/test', (req, res) => {
   res.json({ message: '🎉 Server working!' });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
-});
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
